9c6b08246a7d0eb36c7357176ccadaa22329b6f9
Luis Averhoff <laver008@fiu.edu>
2017-03-01 14:54:50 -0500

    Added user input and a function to check if the input is an integer.

--- MazeGenerator/MazeGenerator.cpp
+++ MazeGenerator/MazeGenerator.cpp
@@ -1,13 +1,44 @@
 // MazeGenerator.cpp : Defines the entry point for the console application.
 #include "Maze.hpp"
-#include <regex>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+int getInt(std::string prompt);
 
 int main()
 {
-	Maze maze(9, 17);
+	int totalRows, totalColumns;
+
+	totalRows = getInt("How many rows do you want the maze to have? ");
+	totalColumns = getInt("How many columns do you want the maze to have? ");
+	std::cout << std::endl;
+
+	Maze maze(totalRows, totalColumns);
 	maze.generateMaze();
 	maze.displayMaze();
+
 	system("pause");
+
     return 0;
+}
+
+int getInt(std::string prompt)
+{
+	int retInteger;
+	std::string strNumber;
+
+	while (true)
+	{
+		std::cout << prompt;
+		std::getline(std::cin, strNumber);
+		std::stringstream convert(strNumber);
+
+		if (convert >> retInteger && !(convert >> strNumber))
+			return retInteger;
+
+		std::cin.clear();
+		std::cerr << "Input must be an integer with only one argument! Please try again." << std::endl << std::endl;
+	}
 }
 
